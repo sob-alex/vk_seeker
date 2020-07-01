373cbc96dd87c47edbff2384179e60754c07cb1f
sob-alex <67511934+sob-alex@users.noreply.github.com>
2020-07-01 14:37:38 +0300

    Removing comments from index.js

--- index.js
+++ index.js
@@ -22,27 +22,16 @@ function createWindow() {
   win.webContents.openDevTools();
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Некоторые API могут использоваться только после возникновения этого события.
 app.whenReady().then(createWindow);
 
-// Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // Для приложений и строки меню в macOS является обычным делом оставаться
-  // активными до тех пор, пока пользователь не выйдет окончательно используя Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
 app.on('activate', () => {
-  // На MacOS обычно пересоздают окно в приложении,
-  // после того, как на иконку в доке нажали и других открытых окон нету.
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
 });
-
-// In this file you can include the rest of your app's specific main process
-// co
